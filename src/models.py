ee67fe326b02c4fc5ca29572fad8161581db2c98
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-08-12 00:16:46 +0000

    up

--- src/models.py
+++ src/models.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# usuario
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -17,3 +19,47 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
+    # Aqui va Planeta 
+
+    class Planets(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(80), nullable=False)
+        url = db.Column(db.String(250), nullable=False)
+        diameter = db.Column(db.String(250), nullable=False)
+        population = db.Column(db.String(250), nullable=False)
+        climate = db.Column(db.String(250), nullable=False)
+        terrain = db.Column(db.String(250), nullable=False)
+        surfaceWater = db.Column(db.String(250), nullable=False)
+        rotationPeriod = db.Column(db.String(250), nullable=False)
+        orbitalPeriod = db.Column(db.String(250), nullable=False)
+        gravity = db.Column(db.String(250), nullable=False)
+        films = db.Column(db.String(250), nullable=False)
+        created = db.Column(db.String(250), nullable=False)
+        edited = db.Column(db.String(250), nullable=False)
+        favorites = db.relationship('Favorites', backref='planets', lazy=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+            "diameter": self.diameter,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surfaceWater": self.surfaceWater,
+            "rotationPeriod": self.rotationPeriod,
+            "orbitalPeriod": self.orbitalPeriod,
+            "gravity": self.gravity,
+            "films": self.films,
+            "created": self.created,
+            "edited": self.edited,
+        }
+
+
+
