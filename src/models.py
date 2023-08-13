430e449870903c6312887543682d4235252729e3
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-08-13 18:55:19 +0000

    up

--- src/models.py
+++ src/models.py
@@ -6,21 +6,23 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
     
- # Aqui va Planeta 
+    
+# Aqui va Planeta 
 
 class Planets(db.Model):
      id = db.Column(db.Integer, primary_key=True)
@@ -62,9 +64,64 @@ def serialize(self):
 
 
 #Personajes
-class Characters (db.Model):
-    id= db.column()
-    name= db.column()
-    url= db.column()
-    species= db.column()
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    url = db.Column(db.String(250), nullable=False)
+    species = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    birthYear = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.String(250), nullable=False)
+    mass = db.Column(db.String(250), nullable=False)
+    hairColor = db.Column(db.String(250), nullable=False)
+    eyeColor = db.Column(db.String(250), nullable=False)
+    skinColor = db.Column(db.String(250), nullable=False)
+    films = db.Column(db.String(250), nullable=False)
+    created = db.Column(db.String(250), nullable=False)
+    edited = db.Column(db.String(250), nullable=False)
+    favorites = db.relationship('Favorites', backref='characters', lazy=True)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+            "species": self.species,
+            "gender": self.gender,
+            "birthYear": self.birthYear,
+            "height": self.height,
+            "mass": self.mass,
+            "hairColor": self.hairColor,
+            "eyeColor": self.eyeColor,
+            "skinColor": self.skinColor,
+            "films": self.films,
+            "created": self.created,
+            "edited": self.edited
+        }
     
+# Favoritos
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    
+
+    
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "characters_id": self.characters_id,
+            "planets_id": self.planets_id,
+        }
+
+
